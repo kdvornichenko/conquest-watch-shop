82281053433d0072171fee36420bfd1563d8c596
kdvornichenko <gglade999@gmail.com>
2023-02-17 19:24:07 +0300

    popular models

--- src/scripts/mobileMenuToggle.js
+++ src/scripts/mobileMenuToggle.js
@@ -4,14 +4,18 @@ const mobileMenuToggle = () => {
 	const tools = document.querySelector('.tools')
 	const blur = document.querySelector('.blur')
 
-	burger.addEventListener('click', () => {
+	function mobileMenu() {
 		burger.classList.toggle('active')
 		mobMenu.classList.toggle('active')
 		blur.classList.toggle('active')
 		tools.classList.toggle('mobile')
 
 		document.body.classList.toggle('overflow-hidden')
-	})
+		document.documentElement.classList.toggle('overflow-hidden')
+	}
+
+	burger.addEventListener('click', () => mobileMenu())
+	blur.addEventListener('click', () => mobileMenu())
 }
 
 export default mobileMenuToggle
