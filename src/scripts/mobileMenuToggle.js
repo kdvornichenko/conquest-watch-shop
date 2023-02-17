dbb598cd0fbe96884e65db110e14200aca410a64
kdvornichenko <gglade999@gmail.com>
2023-02-17 16:22:10 +0300

    first screen responsive

--- src/scripts/mobileMenuToggle.js
+++ src/scripts/mobileMenuToggle.js
@@ -2,10 +2,12 @@ const mobileMenuToggle = () => {
 	const burger = document.querySelector('.burger')
 	const mobMenu = document.querySelector('.mobile-menu')
 	const tools = document.querySelector('.tools')
+	const blur = document.querySelector('.blur')
 
 	burger.addEventListener('click', () => {
 		burger.classList.toggle('active')
 		mobMenu.classList.toggle('active')
+		blur.classList.toggle('active')
 		tools.classList.toggle('mobile')
 
 		document.body.classList.toggle('overflow-hidden')
